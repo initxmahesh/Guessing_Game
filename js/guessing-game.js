728477fe2629d53edb42abf0aa0e374369f2cb1b
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-18 18:22:38 +0545

    guessing game

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -7,3 +7,77 @@ In this file, you will also include the event listeners that are needed to inter
 a user clicks a button or adds a guess to the input field.
 
 */
+
+function generateWinningNumber (){
+    return Math.ceil(Math.random() * 100);
+}
+
+function shuffle(array) {
+  let length = array.length,
+    temp,
+    index;
+  while (length) {
+    index = Math.floor(Math.random() * length--);
+    temp = array[length];
+    array[length] = array[index];
+    array[index] = temp;
+  }
+  return array;
+}
+
+class Game {
+  constructor(playersGuess = null, pastGuesses = []) {
+    this.playersGuess = playersGuess;
+    this.pastGuesses = pastGuesses;
+    this.winningNumber = generateWinningNumber();
+  }
+
+  difference() {
+    return Math.abs(this.playersGuess - this.winningNumber);
+  }
+
+  isLower() {
+    if (this.playersGuess < this.winningNumber) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  playersGuessSubmission(num) {
+    if (num < 1 || num > 100 || typeof num !== "number") {
+      throw `That is an invalid guess.`;
+    } else {
+      this.playersGuess = num;
+    }
+    return this.checkGuess();
+  }
+
+  checkGuess() {
+    if (this.playersGuess === this.winningNumber) return `You Win!`;
+    else if (this.pastGuesses.includes(this.playersGuess))
+      return `You have already guessed that number.`;
+    else this.pastGuesses.push(this.playersGuess);
+
+    if (this.pastGuesses.length === 5) return `You Lose.`;
+
+    let guesslvl = Math.abs(this.playersGuess - this.winningNumber);
+    if (guesslvl < 10) return `You're burning up!`;
+    if (guesslvl < 25) return `You're lukewarm.`;
+    if (guesslvl < 50) return `You're a bit chilly.`;
+    if (guesslvl < 100) return `You're ice cold!`;
+  }
+
+  provideHint() {
+    let hintArray = [];
+    hintArray.push(this.winningNumber);
+    hintArray.push(generateWinningNumber());
+    hintArray.push(generateWinningNumber());
+
+    return shuffle(hintArray);
+  }
+}
+
+let newGame = function () {
+  return new Game();
+};
